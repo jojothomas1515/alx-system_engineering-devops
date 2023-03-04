6be4743d6d8c669a8eaabc4299281eee9eb9db99
Jojo <jojothomas1515@gmail.com>
2023-03-04 12:49:25 +0100

    zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - infinite loop
+ * Return: 0 always
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - extry point of my program
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+	__pid_t process, status;
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		process = fork();
+		if (process == -1)
+			break;
+		if (process == 0)
+		{
+			printf("The pid of the process is = %d, the parent pid is = %d \n",
+			       getpid(), getppid());
+			exit(1);
+		}
+	}
+	infinite_while();
+}
