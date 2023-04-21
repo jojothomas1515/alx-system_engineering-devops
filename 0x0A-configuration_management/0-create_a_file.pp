c5e6207e7f2b67920a29df8797034fbffdbdc33b
Jojo <jojothomas1515@gmail.com>
2023-04-22 00:59:54 +0100

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,7 @@
 # create a file named school in the temp folder
 file { '/tmp/school':
+  ensure   => file,
+  checksum => 'md5',
   content  => 'I love Puppet',
   mode     => '0744',
   owner    => 'www-data',
