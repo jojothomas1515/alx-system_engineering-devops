870f2018210e1b9e2c7722290f13d4bae86f78c1
Jojo <jojothomas1515@gmail.com>
2023-05-22 21:47:27 +0100

    exporting data from api to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Rest Api."""
+
+import requests
+import sys
+
+
+def get_info_to_csv(user_id: int):
+    """
+    Search for a user and print out all completed tasks.
+
+    args:
+        user_id: the id if the user to search for
+    """
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(user_id))
+    info = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(user_id))
+    name = user.json()['name']
+    todos = info.json()
+    inp = "{},{},{},{}\n"
+    with open("{}.csv".format(user_id), "w") as f:
+        for task in todos:
+            f.write(
+                inp
+                .format(user_id,
+                        name,
+                        task['completed'],
+                        task['title']))
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print("usage {} <user_id>"
+              .format(__file__))
+    else:
+        get_info_to_csv(int(sys.argv[1]))
