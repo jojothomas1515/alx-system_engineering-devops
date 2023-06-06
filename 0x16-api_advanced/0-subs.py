164c519ffada6e2e43188355776806592ea308bc
Jojo <jojothomas1515@gmail.com>
2023-06-06 21:32:57 +0100

    get subreddits subscibers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,6 @@
 """ Subreddit subscribers."""
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
@@ -13,24 +12,13 @@ def number_of_subscribers(subreddit):
     Args:
         subreddit (str): the subreddit to get the info of
     """
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url,
                             allow_redirects=False,
                             headers={'User-Agent': 'Mozilla/5.0'})
-    print(response.history)
     if response.status_code == 200:
         data = response.json()
         subscibers_count = data.get("data").get("subscribers")
         return (subscibers_count)
     else:
         return (0)
-
-
-if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) != 2:
-        print("This program only accepts one parameter")
-        exit(-1)
-    print(number_of_subscribers(argv[1]))
