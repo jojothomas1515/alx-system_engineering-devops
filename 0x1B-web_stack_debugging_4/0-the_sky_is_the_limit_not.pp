089baa71208027a6eb88c0461f6f4bf2778ad2fc
Jojo <jojothomas1515@gmail.com>
2023-06-16 15:58:08 +0100

    puppet fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -7,7 +7,7 @@ service {'nginx':
   }
 
 exec {'limit':
-  command => 'sed -i "s/#ULIMIT=\"-n 4096\"/ULIMIT=\"-n 4096\"" /etc/default/nginx',
+  command => 'sed -i "s/ULIMIT=\"-n 4096\"/ULIMIT=\"-n 4096\"" /etc/default/nginx',
   path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
   notify  => Service['nginx'],
   }
