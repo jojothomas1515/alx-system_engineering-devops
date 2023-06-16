ae6a9dca7e8e3ecb26e50a244237f3222843d9a5
Jojo <jojothomas1515@gmail.com>
2023-06-16 18:46:16 +0100

    puppet user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # remove holberton user limit
 
 exec {'remove limit for holberton':
-  command  => 'sed -i "/holberton/d" /etc/security/limits.conf'
+  command  => 'sed -i "/holberton/d" /etc/security/limits.conf',
   provider => shell,
   }
